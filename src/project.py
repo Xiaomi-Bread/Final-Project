65c2b398c960548e14b8e6a10a33226fc90bf659
Xiaomi-Bread <Alangao314@gmail.com>
2024-05-05 17:27:54 -0500

    V2- Added a Png to the directory and also into pygame screen.

--- src/project.py
+++ src/project.py
@@ -2,16 +2,33 @@ import pygame
 
 pygame.init 
 
+#Main Resolution
 screen_width = 1550
 screen_height = 790 
 screen = pygame.display.set_mode((screen_width, screen_height))
+WHITE = (255, 255, 255)
 
+#Images for the game
+player_model = pygame.image.load("WW1Tank.png").convert_alpha()
+
+player_model_width = 230 
+player_model_height = 160 
+
+x_player = screen_width // 3 - player_model_width //2 
+y_player = screen_height - 450
+
+#Game Loop
 running = True
 while running: 
     for event  in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False 
+    screen.fill(WHITE)
 
-pygame.time.Clock().tick(24)
+    screen.blit(player_model, (x_player, y_player))
+
+    pygame.display.flip()
+
+    pygame.time.Clock().tick(24)
 
 pygame.quit()
