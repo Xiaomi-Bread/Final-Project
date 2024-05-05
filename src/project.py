5503188ce79a152368dd1876f4cd5c6da79413b9
Xiaomi-Bread <Alangao314@gmail.com>
2024-05-05 18:03:41 -0500

    V-4 Coded Player movement and also player hitbox

--- src/project.py
+++ src/project.py
@@ -11,6 +11,9 @@ WHITE = (255, 255, 255)
 player_model_width = 230 
 player_model_height = 170 
 
+player_hitbox_width = 140
+player_hitbox_height = 80
+
 #Images for the game
 player_model = pygame.image.load("WW1Tank.png").convert_alpha()
 player_model = pygame.transform.scale(player_model, (player_model_width, player_model_height))
@@ -18,18 +21,51 @@ player_model = pygame.transform.scale(player_model, (player_model_width, player_
 x_player = screen_width // 3 - player_model_width //2 
 y_player = screen_height - 450
 
+hitbox_vertical_shift = 15
+
+player_model_x = x_player
+player_model_y = y_player 
+
+player_hitbox_x = x_player + (player_model_width - player_hitbox_width) // 2 
+player_hitbox_y = y_player + (player_hitbox_height - player_hitbox_height) //2 + hitbox_vertical_shift
+
+player_rectangle = pygame.Rect(player_hitbox_x, player_hitbox_y, player_hitbox_width, player_hitbox_height)
+
+player_speed = 3
+
+def update_player(): 
+    global x_player, y_player, player_rectangle 
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w] or keys[pygame.K_UP]:
+        y_player -= player_speed 
+    if keys[pygame.K_s] or keys[pygame.K_DOWN]:
+        y_player += player_speed 
+
+    player_hitbox_x  = x_player + (player_model_width - player_hitbox_width) //2 
+    player_hitbox_y = y_player +(player_model_height - player_hitbox_height) //2 + hitbox_vertical_shift
+    player_rectangle.x = player_hitbox_x
+    player_rectangle.y = player_hitbox_y
+
+def draw_hitbox():
+    pygame.draw.rect(screen, (255, 0, 0), player_rectangle, 2)
+
 #Game Loop
 running = True
 while running: 
     for event  in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False 
+
+    update_player()
+
     screen.fill(WHITE)
 
     screen.blit(player_model, (x_player, y_player))
 
+    draw_hitbox()
+
     pygame.display.flip()
 
-    pygame.time.Clock().tick(24)
+    pygame.time.Clock().tick(60)
 
 pygame.quit()
